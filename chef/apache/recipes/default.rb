d5e58bdc10a066b3f43ee48c5cf8a79ea3c2410a
Niko Smeds <nikosmeds@gmail.com>
2018-04-18 22:02:09 -0700

    [chef] Update default.rb

--- chef/apache/recipes/default.rb
+++ chef/apache/recipes/default.rb
@@ -1,8 +1,23 @@
-package 'ntp'
-package 'apache2'
-
 hostname = 'replace_with_ohai_lookup'
+
+package 'apache2' do
+  version '2.4.18-2ubuntu3.5'
+  action :install
+end
+
+%w{curl ntp tree vim}.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+service 'apache2' do
+  subscribes :reload, 'file[var/www/html/index.html]', :immediately
+end
 
 file '/var/www/html/index.html' do
   content "#{hostname}\n"
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
